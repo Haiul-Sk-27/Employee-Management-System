18f6b62dcc215889edd0d8cd51faf95889c9b6f1
Haiul sk <HaiulSk037@Gmail.com>
2025-10-02 01:48:39 +0530

    EM_System

--- src/Login.java
+++ src/Login.java
@@ -1,34 +1,38 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener{
 
-    Login(){
+    JTextField tfusername, tfpassword;
 
-        getContentPane().setBackground(Color.white);
+    Login() {
+
+        getContentPane().setBackground(Color.WHITE);
         setLayout(null);
 
-        JLabel lblUserName = new JLabel("UserName");
-        lblUserName.setBounds(40,20,100,30);
-        add(lblUserName);
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(40, 20, 100, 30);
+        add(lblusername);
 
-        JTextField tfuserName = new JTextField();
-        tfuserName.setBounds(150,20,150,30);
-        add(tfuserName);
+        tfusername = new JTextField();
+        tfusername.setBounds(150, 20, 150, 30);
+        add(tfusername);
 
-        JLabel lnlPassword = new JLabel("password");
-        lnlPassword.setBounds(40,70,100,30);
-        add(lnlPassword);
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(40, 70, 100, 30);
+        add(lblpassword);
 
-        JTextField tfpassword = new JTextField();
-        tfpassword.setBounds(150,70,150,30);
+        tfpassword = new JTextField();
+        tfpassword.setBounds(150, 70, 150, 30);
         add(tfpassword);
 
         JButton login = new JButton("LOGIN");
-        login.setBounds(150,140,150,30);
-        login.setBackground(Color.black);
-        login.setForeground(Color.white);
+        login.setBounds(150, 140, 150, 30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.addActionListener(this);
         add(login);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
@@ -38,12 +42,33 @@ public class Login extends JFrame {
         image.setBounds(350, 0, 200, 200);
         add(image);
 
-        setSize(600,300);
-        setLocation(450,200);
+        setSize(600, 300);
+        setLocation(450, 200);
         setVisible(true);
     }
 
-    public static void main (String[] args){
+    public void actionPerformed(ActionEvent ae) {
+        try {
+            String username = tfusername.getText();
+            String password = tfpassword.getText();
+
+            Conn c = new Conn();
+            String query = "select * from login where username = '"+username+"' and password = '"+password+"'";
+
+            ResultSet rs = c.s.executeQuery(query);
+            if (rs.next()) {
+                setVisible(false);
+//                new Home();
+            } else {
+                JOptionPane.showMessageDialog(null, "Invalid username or password");
+                setVisible(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
         new Login();
     }
 }
